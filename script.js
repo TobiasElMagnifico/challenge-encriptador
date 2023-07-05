22f6b42fa308e274ead487aef9029b2ada2dff70
tobias <tobiaselmagnifico@gmail.com>
2023-07-05 14:59:27 -0300

    agrege comportamiento a boton copiar

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var textarea = document.querySelector('textarea');
+var btnEncriptar = document.getElementById("btn-encriptar");
+var btnDesencriptar = document.getElementById("btn-desencriptar");
+var btnCopiar = document.getElementById("btn-copiar");
 var indice=0;
 textarea.focus();
 var caracteresProhibidos = /[^a-z\s]/g; //cualquier caracter excepto minúsculas y espacios.
 
 function encriptar() {
     
-    var letras = [/a/g,/e/g,/i/g,/o/g,/u/g,/aimes/g];
-    var codigos = ["ai","enter","imes","ober","ufat","ai"];
-    var textoSinEncriptar = textarea.value;
+    var letras = [/e/g,/i/g,/a/g,/o/g,/u/g]; //lo puse en este orden para que la itineración secuencial respete los requisitos del desafio 
+    var codigos = ["enter","imes","ai","ober","ufat"];
+    var textoSinEncriptar = textarea.value; //evalue sacarlo de la función para ahorrar código pero aún no pude hacerlo viable.
 
     if ((caracteresProhibidos.test(textoSinEncriptar))) {
         alert("Usted ha ingresado un caracter prohibido.");
@@ -17,6 +20,7 @@ function encriptar() {
             textoSinEncriptar=textoEncriptado;
             indice++;
         }
+        indice=0;
         mostrarAreaMensaje(textoEncriptado);
     }
 }
@@ -31,19 +35,27 @@ function desencriptar() {
         textoEncriptado=textoDesencriptado;
         indice++;
     }
+    indice=0;
     mostrarAreaMensaje(textoDesencriptado);
 }
 
-function mostrarAreaMensaje (funcion) {
+function mostrarAreaMensaje (texto) {
     
     document.getElementById("output-default").style.display="none";
     document.getElementById("output-texto").style.display="block";
-    document.getElementById("demo").innerHTML = funcion;
+    document.getElementById("div-resultado").innerHTML = texto;
+}
+
+function copiarTexto () {
+    
+    var copyText = document.getElementById("div-resultado").textContent;
+    textarea.select(); //ojo que capaz es al pedo esto (selecciona texto automaticamnte)
+    navigator.clipboard.writeText(copyText);
+    alert("¡Copiado!");
+
 }
     
-
-var btnEncriptar = document.getElementById("btn-encriptar");
-var btnDesencriptar = document.getElementById("btn-desencriptar");
 btnEncriptar.onclick= encriptar;
 btnDesencriptar.onclick = desencriptar;
+btnCopiar.onclick = copiarTexto;
 
