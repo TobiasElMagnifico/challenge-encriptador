02b8f534c47d0896ec1946142f708c87b0eee80f
tobias <tobiaselmagnifico@gmail.com>
2023-07-08 10:10:48 -0300

    agregué README y meta description

--- script.js
+++ script.js
@@ -3,25 +3,34 @@ var btnEncriptar = document.getElementById("btn-encriptar");
 var btnDesencriptar = document.getElementById("btn-desencriptar");
 var btnCopiar = document.getElementById("btn-copiar");
 var indice=0;
-textarea.focus();
 var caracteresProhibidos = /[^a-z\s]/g; //cualquier caracter excepto minúsculas y espacios.
+textarea.focus();
 
 function encriptar() {
     
     var letras = [/e/g,/i/g,/a/g,/o/g,/u/g]; //lo puse en este orden para que la itineración secuencial respete los requisitos del desafio 
     var codigos = ["enter","imes","ai","ober","ufat"];
     var textoSinEncriptar = textarea.value; //evalue sacarlo de la función para ahorrar código pero aún no pude hacerlo viable.
-
-    if ((caracteresProhibidos.test(textoSinEncriptar))) {
-        alert("Usted ha ingresado un caracter prohibido.");
+    var testTexto = caracteresProhibidos.test(textoSinEncriptar);
+    
+    if(textoSinEncriptar == "") {
+        textarea.focus();
     } else {
-        while(indice<letras.length) {
-            var textoEncriptado = textoSinEncriptar.replace(letras[indice],codigos[indice]);
-            textoSinEncriptar=textoEncriptado;
-            indice++;
+
+        if ((testTexto)) {
+            alert("Usted ha ingresado un caracter inválido. Corrija su texto y vuela a intentarlo");
+        } else {
+            while(indice<letras.length) {
+                var textoEncriptado = textoSinEncriptar.replace(letras[indice],codigos[indice]);
+                textoSinEncriptar=textoEncriptado;
+                indice++;
+            }
+            indice=0;
+            mostrarAreaMensaje(textoEncriptado);
+            //window.scroll({top:500,behavior:"smooth"}); 
+            //función para scrollear automaticamente hasta resultado
+            
         }
-        indice=0;
-        mostrarAreaMensaje(textoEncriptado);
     }
 }
 
@@ -30,29 +39,35 @@ function desencriptar() {
     var letras = ["a","e","i","o","u"];
     var codigos = [/ai/g,/enter/g,/imes/g,/ober/g,/ufat/g];
     var textoEncriptado = textarea.value;
-    while(indice<letras.length) {
-        var textoDesencriptado = textoEncriptado.replace(codigos[indice],letras[indice]);
-        textoEncriptado=textoDesencriptado;
-        indice++;
+
+    if(textoEncriptado == "") {
+        textarea.focus();
+    } else {
+        while(indice<letras.length) {
+            var textoDesencriptado = textoEncriptado.replace(codigos[indice],letras[indice]);
+            textoEncriptado=textoDesencriptado;
+            indice++;
+        }
+        indice=0;
+        mostrarAreaMensaje(textoDesencriptado);
+        // window.scroll({top:600,behavior:"smooth"});
+        //función para scrollear automaticamente hasta resultado
     }
-    indice=0;
-    mostrarAreaMensaje(textoDesencriptado);
 }
 
 function mostrarAreaMensaje (texto) {
     
     document.getElementById("output-default").style.display="none";
     document.getElementById("output-texto").style.display="block";
-    document.getElementById("div-resultado").innerHTML = texto;
+    document.getElementById("textarea-resultado").innerHTML = texto;
 }
 
 function copiarTexto () {
     
-    var copyText = document.getElementById("div-resultado").textContent;
+    var copyText = document.getElementById("textarea-resultado").textContent;
     textarea.select(); //ojo que capaz es al pedo esto (selecciona texto automaticamnte)
     navigator.clipboard.writeText(copyText);
     alert("¡Copiado!");
-
 }
     
 btnEncriptar.onclick= encriptar;
